becef0a35c496ff9e3756e537c3666dc617e9d4d
Roman <romanbezuhlyi@gmail.com>
2023-02-27 19:38:35 +0200

    Добавлена модель в БД Настроена панель администратора

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Назва', max_length=100)
+    task = models.TextField('Завдання')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Завдання'
+        verbose_name_plural = 'Всі завдання'
